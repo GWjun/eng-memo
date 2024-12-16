3d5e5807da24eca269b59e4401609b4f6fd417f5
GWJun <kb865900@gmail.com>
2024-12-16 22:48:33 +0900

    ㄴãfeat: Button component

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import ui.common.Button;
+
+public class Main extends JFrame {
+
+  Main() {
+    setTitle("English Master");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+    setIconImage(new ImageIcon("src/resource/images/logo.png").getImage());
+
+    Container c = getContentPane();
+    c.setLayout(null);
+
+    Button loginButton = new Button("Login", "md", "primary");
+    loginButton.setLocation(160, 700);
+
+    Button registerButton = new Button("Register", "sm", "text");
+    registerButton.setSize(90, 28);
+    registerButton.setLocation(155, 750);
+
+    c.add(loginButton);
+    c.add(registerButton);
+
+    setSize(400, 900);
+    setVisible(true);
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    new Main();
   }
 }
---
+++ src/ui/common/Button.java
@@ -0,0 +1,68 @@
+package ui.common;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class Button extends JButton {
+
+  private String size;
+  private String variant;
+  private Color pressedBackgroundColor;
+
+  public Button(String text, String size, String variant) {
+    super(text);
+    this.size = size;
+    this.variant = variant;
+    this.pressedBackgroundColor = new Color(214, 214, 229);
+    applyStyles();
+  }
+
+  public Button(String text, String size, String variant, ActionListener onClick) {
+    this(text, size, variant);
+    this.addActionListener(onClick);
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    if (getModel().isPressed() && pressedBackgroundColor != null) {
+      g.setColor(pressedBackgroundColor);
+    } else {
+      g.setColor(getBackground());
+    }
+    g.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+    super.paintComponent(g);
+  }
+
+  private void applyStyles() {
+    setContentAreaFilled(false); // for custom color
+    setBorderPainted(false);
+
+    switch (variant) {
+      case "primary" -> {
+        setBackground(new Color(66, 133, 244));
+        setForeground(Color.white);
+        this.pressedBackgroundColor = new Color(12, 87, 205);
+      }
+      case "secondary" -> setBackground(new Color(169, 198, 255));
+      case "destructive" -> {
+        setBackground(new Color(234, 67, 53));
+        setForeground(Color.white);
+        this.pressedBackgroundColor = new Color(180, 21, 21);
+      }
+      case "outline" -> {
+        setBorder(new RoundedBorder(15));
+        setBorderPainted(true);
+      }
+      case "text" -> {
+        this.pressedBackgroundColor = null;
+      }
+    }
+
+    switch (size) {
+      case "sm" -> setSize(60, 28);
+      case "md" -> setSize(80, 35);
+      case "lg" -> setSize(100, 40);
+    }
+  }
+}
